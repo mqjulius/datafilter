09b4fa964b8202ce38bfcaada9fe0a0042f41e17
MqJulius <julius.hundt.1997@gmail.com>
2022-11-21 11:17:47 +0100

    Update datafilter.py

--- datafilter.py
+++ datafilter.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Datenfilter")
+st.markdown("Beschreibung")
+
+
+#DF Load Cache 
+def load_data():
+  path = ""
+  return pd.read_excel("path_to_file.xls", sheet_name="Sheet1")
+df = load_data()
+
+#DF Filter
+df = df[["StartTime"]]
+
+#Sidebar für Filter
+st.sidebar.header('Filter')
+st.sidebar.slider
